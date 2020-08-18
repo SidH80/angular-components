85c0ffae451e9d16ab0e746ac5a7a2d5f2f40e63
lethalSid <senien.hicks@gmail.com>
2020-08-17 23:34:52 -0400

    committing massive adjustments

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
-
+  data = [
+    { name: 'James', age: 24, job: 'Engineer', employeed: true },
+    { name: 'Jill', age: 26, job: 'Developer', employeed: false },
+    { name: 'Elyse', age: 25, job: 'Designer', employeed: true },
+  ];
+  headers = [
+    { key: 'employeed', label: 'Has a job?' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
   constructor() { }
 
   ngOnInit() {
