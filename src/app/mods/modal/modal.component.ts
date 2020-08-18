d71384e226f7f9e9cfa89798922f5ec88a4ab453
lethalSid <senien.hicks@gmail.com>
2020-08-18 10:22:07 -0400

    added modal with logic

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
+  @Output() close = new EventEmitter;
+  constructor(
+    private el: ElementRef,
 
-  constructor(private el: ElementRef) {
+  ) {
+  }
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
   }
 
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  onCloseClick() {
+    this.close.emit();
   }
 
 }
